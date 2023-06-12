c0a9958c465091d75e0fb7a6fa4c1e853ce6cc9e
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-06-12 16:40:04 +0100

    tryna get the brand StoryScape to sit to the left

--- StoryScape-FrontEnd/src/components/navBar/navBar.jsx
+++ StoryScape-FrontEnd/src/components/navBar/navBar.jsx
@@ -11,23 +11,36 @@ import { FaRegHandshake } from 'react-icons/fa';
 import { ImProfile } from 'react-icons/im';
 
 export default function NavBar() {
-    return ( 
-        
-<Navbar className="nav" collapseOnSelect expand="lg" >
-<Container className="navBarContainer">
-  <Navbar.Brand className='brand' href="#home">Story Scape</Navbar.Brand>
-  <Nav.Link className='navElement' href="#ourstory"> <ImProfile /> Profile </Nav.Link>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="linkContainer">
-      <Nav.Link className='navElement' href="#home"> <FcHome /> Home</Nav.Link>
-      <Nav.Link className='navElement' href="#map"> <FcGlobe /> Map</Nav.Link>
-      <Nav.Link className='navElement' href="#post"> <BsFillEnvelopeFill /> Post</Nav.Link>
-      <Nav.Link className='navElement' href="#stories"> <GoBook /> Stories</Nav.Link>
-      <Nav.Link className='navElement' href="#ourstory"> <FaRegHandshake /> Our Story</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Container>
-</Navbar>
-)
-} 
+  return (
+    <Navbar className="nav" collapseOnSelect expand="lg">
+      <Container className="navBarContainer">
+        <Navbar.Brand className="brand" href="#home">
+          Story Scape
+        </Navbar.Brand>
+        <Nav.Link className="navElement profileLink" href="#ourstory">
+          <ImProfile /> Profile
+        </Nav.Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="linkContainer">
+            <Nav.Link className="navElement" href="#home">
+              <FcHome /> Home
+            </Nav.Link>
+            <Nav.Link className="navElement" href="#map">
+              <FcGlobe /> Map
+            </Nav.Link>
+            <Nav.Link className="navElement" href="#post">
+              <BsFillEnvelopeFill /> Post
+            </Nav.Link>
+            <Nav.Link className="navElement" href="#stories">
+              <GoBook /> Stories
+            </Nav.Link>
+            <Nav.Link className="navElement" href="#ourstory">
+              <FaRegHandshake /> Our Story
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
