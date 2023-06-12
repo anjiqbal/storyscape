088c3b01a52c8c509832ccba6fb394640fa648de
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-06-12 11:47:41 +0100

    added search component

--- StoryScape-FrontEnd/src/components/App/App.jsx
+++ StoryScape-FrontEnd/src/components/App/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import Search from "../search/search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+  
 
   return (
     <>
-      <div>
-       "Hello" 
-      </div>
+      <div>"Hello"</div>
+      <Search setSearchTerm={setSearchTerm} />
       <h1>This is our APP</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-     </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ StoryScape-FrontEnd/src/components/search/search.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+
+function Search({ setSearchTerm }) {
+  //const navigate = useNavigate();
+  const [input, setInput] = useState("");
+  function handleInput(input) {
+    setInput(input);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearchTerm(input);
+    setInput("");
+
+    // navigate("/cardCollection", { state: input });
+    //We set the state to input because setSearchTerm has not yet updated the state to include the new input
+  }
+
+  function handleEnter(event) {
+    if (event.key === "Enter") {
+      setSearchTerm(input);
+      setInput("");
+    }
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a location"
+          value={input}
+          onChange={(event) => {
+            handleInput(event.target.value);
+          }}
+          onKeyDown={handleEnter}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </section>
+  );
+}
+
+export default Search;
