943a75307ba1f25c865c599a05def0ecc598ef29
AkielCh <akiel98@hotmail.com>
2023-06-13 15:06:59 +0100

    Model for Description is complete

--- StoryScape-FrontEnd/src/components/uploadForm/textUpload/Modal/Modal.jsx
+++ StoryScape-FrontEnd/src/components/uploadForm/textUpload/Modal/Modal.jsx
@@ -6,6 +6,9 @@ import Form from 'react-bootstrap/Form';
 function ModalForText() {
     const [show, setShow] = useState(false);
     const [description, setDescription] = useState('');
+    const [text, setText] = useState('');
+    const max_text_length = 200;
+  
   
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -16,8 +19,25 @@ function handleCloseSave() {
         }
 console.log(description);
   
+
+const handleChange = (event) => {
+    const text_length = event.target.value.length;
+    const remaining_text = max_text_length - text_length;
+    setText(event.target.value);
+    document.getElementById("count_message").innerHTML = remaining_text + " characters remaining";
+  };
+
     return (
       <>
+
+      <div className="col-sm-4 well">
+        <form acceptCharset="UTF-8" action="" method="POST">
+          <textarea readOnly className="form-control" id="text" name="text" maxLength="200" placeholder="Please add a short description of your video" rows="5" value={text} onChange={handleChange}></textarea>
+          <span className="pull-right label label-default" id="count_message">{text.length} / {max_text_length}</span>
+          {/* <br/>
+          <button className="btn btn-info" type="submit">Post New Message</button> */}
+        </form>
+
         <Button variant="primary" onClick={handleShow}>
             EDIT
         </Button>
@@ -46,8 +66,11 @@ console.log(description);
             </Button>
           </Modal.Footer>
         </Modal>
+        </div>
       </>
+
     );
+
   }
   
   export default ModalForText;
--- StoryScape-FrontEnd/src/components/uploadForm/textUpload/textUpload.jsx
+++ StoryScape-FrontEnd/src/components/uploadForm/textUpload/textUpload.jsx
@@ -1,32 +1,81 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './textUpload.css';
-import ModalForText from './Modal/Modal';
+
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function TextUpload() {
-  const [text, setText] = useState('');
-  const max_text_length = 200;
+    const [show, setShow] = useState(false);
+    const [description, setDescription] = useState('');
+    const [text, setText] = useState('');
+    const max_text_length = 200;
+  
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  const handleChange = (event) => {
+function handleCloseSave() {
+    setDescription(document.getElementById("exampleForm.ControlTextarea1").value);
+    setShow(false);
+        }
+console.log(description);
+  
+
+const handleChange = (event) => {
     const text_length = event.target.value.length;
     const remaining_text = max_text_length - text_length;
     setText(event.target.value);
     document.getElementById("count_message").innerHTML = remaining_text + " characters remaining";
   };
 
-  return (
-    <>
+    return (
+      <>
+
       <div className="col-sm-4 well">
         <form acceptCharset="UTF-8" action="" method="POST">
-          <textarea readOnly className="form-control" id="text" name="text" maxLength="200" placeholder="Please add a short description of your video" rows="5" value={text} onChange={handleChange}></textarea>
-          <span className="pull-right label label-default" id="count_message">{text.length} / {max_text_length}</span>
+          <textarea readOnly className="form-control" id="text" name="text" maxLength="200" placeholder="My story is about" rows="5" value={description} ></textarea>
+          
           {/* <br/>
           <button className="btn btn-info" type="submit">Post New Message</button> */}
         </form>
-        <ModalForText/>
-      </div>
-    </>
-  );
-}
+
+        <Button variant="primary" onClick={handleShow}>
+            EDIT
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Description</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Description</Form.Label>
+                <Form.Control as="textarea" rows={4} onChange={handleChange} />
+                <span className="pull-right label label-default" id="count_message">{text.length} / {max_text_length}</span>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleCloseSave} >
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        </div>
+      </>
+
+    );
+
+  }
 
 export default TextUpload;
