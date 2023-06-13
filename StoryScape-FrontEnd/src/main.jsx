a64f62b2eccda7ffa000277cd9a3603e6c7b7a58
Qas1m0m0 <Qasim.17@hotmail.com>
2023-06-13 12:20:03 +0100

    Routing Completed

--- StoryScape-FrontEnd/src/main.jsx
+++ StoryScape-FrontEnd/src/main.jsx
@@ -8,6 +8,9 @@ import UploadForm from './components/uploadForm/uploadForm.jsx';
 import StoryCollection from './components/storyCollection/storyCollection.jsx';
 import Story from './components/storyCollection/story/story.jsx';
 import AboutUs from './components/aboutUs/aboutUs.jsx';
+import ProfileOwn from './components/userProfile/profileOwn.jsx';
+import ProfileOther from './components/userProfileOther/profileOther.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
     <BrowserRouter>
@@ -19,6 +22,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="storyCollection" element={<StoryCollection />} />
         <Route path="story" element={<Story />} />
         <Route path="aboutUs" element={<AboutUs/>} />
+        <Route path="profileOwn" element={<ProfileOwn/>} />
+        <Route path="profileOther" element={<ProfileOther/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
