5d0bf836526befd97107de6c6ff177b9e9547e5c
Trip25 <t.gurung1995@gmail.com>
2023-06-15 12:35:35 +0100

    near done- just need to link to prob statement page

--- StoryScape-FrontEnd/src/components/storyMissionCard/storyMission.jsx
+++ StoryScape-FrontEnd/src/components/storyMissionCard/storyMission.jsx
@@ -9,12 +9,11 @@ function storyMission() {
     <Container className='mission-container'>
     <img className='mission-image' src={logo} alt=''/>
     <h4 className='mission-title'>Our Mission</h4>
-    <p className='mission-line-1'>To connect the world through our experiences</p>
-    <p className='mission-line-2'>Create a vibrant tapestry with your memories saved for generation after 
-generation. 
-</p>
-    <p className='mission-line-3'><a href=''> Click here</a>&nbsp; to find out more</p>
-    <p className='mission-line-4'>Sincerely,<br />
+    <p className='mission-line-1'>To connect the world through our experiences <br />
+    Create a vibrant tapestry with your memories saved for generation after generation. <br/></p>
+    <p className='mission-line-2'>
+    <a href=''> Click here</a>&nbsp; to find out more</p>
+    <p className='mission-line-3'>Sincerely,<br />
     The StoryScape Team</p>
     <p className='slogan-signoff'>Your past, Our future</p>
     </Container>
