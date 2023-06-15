51dc0e92f193f3c6971c1f1d8379ee94c1d59061
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-06-15 13:29:48 +0100

    picture is small and aligned alltho not responsive

--- StoryScape-FrontEnd/src/components/homePageStory/homePageStory.jsx
+++ StoryScape-FrontEnd/src/components/homePageStory/homePageStory.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
-import './homePageStory.css';
-import image1 from  './images/Story-1.jpg';
+import image1 from './images/Story-1.jpg';
 import image2 from './images/Story-2.jpg';
 import image3 from './images/Story-3.jpg';
+import './HomePageStory.css';
 
 const HomePageStory = () => {
   const [story, setStory] = useState({});
@@ -17,7 +17,7 @@ const HomePageStory = () => {
       paragraph: 'This is the story for image 2.',
     },
     {
-      image: image3, 
+      image: image3,
       paragraph: 'This is the story for image 3.',
     },
   ];
@@ -29,9 +29,9 @@ const HomePageStory = () => {
   }, []);
 
   return (
-    <div>
-      <img src={story.image} alt="Story" />
-      <p>{story.paragraph}</p>
+    <div className="piccontainer">
+      <img src={story.image} alt="Story" className="image" />
+      <p className="text">{story.paragraph}</p>
     </div>
   );
 };
