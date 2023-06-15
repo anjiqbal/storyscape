35f8954ba13ccff155082bab1b140cbcc8652c55
Trip25 <t.gurung1995@gmail.com>
2023-06-15 12:03:47 +0100

    styled the mission statement as prototype- need to fix the anchor and add image

--- StoryScape-FrontEnd/src/components/storyMissionCard/storyMission.jsx
+++ StoryScape-FrontEnd/src/components/storyMissionCard/storyMission.jsx
@@ -2,17 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './storymission.css';
 import Container from 'react-bootstrap/Container';
 
+
 function storyMission() {
   return (
     <Container className='mission-container'>
-    <image />
-    <h4>Our Mission</h4>
-    <p>To connect the world through our experiences</p>
-    <p>Create a vibrant tapestry with your memories saved for generation after 
+    <img className='mission-image' src='' alt=''/>
+    <h4 className='mission-title'>Our Mission</h4>
+    <p className='mission-line-1'>To connect the world through our experiences</p>
+    <p className='mission-line-2'>Create a vibrant tapestry with your memories saved for generation after 
 generation. 
 </p>
-    <p><a href=''>Click here</a> to find out more</p>
-    <p>Sincerely,<br />
+    <p className='mission-line-3'><a href=''> Click here </a> to find out more</p>
+    <p className='mission-line-4'>Sincerely,<br />
     The StoryScape Team</p>
     <p className='slogan-signoff'>Your past, Our future</p>
     </Container>
