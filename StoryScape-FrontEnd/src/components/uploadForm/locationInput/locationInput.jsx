406611f27d77674fc9f9e715de2682642d172b0b
Trip25 <t.gurung1995@gmail.com>
2023-06-19 00:10:11 +0100

    finally got the location search bar working with autocomplete and added a map for fun but will now need to rejig the css and discuss with team

--- StoryScape-FrontEnd/src/components/uploadForm/locationInput/locationInput.jsx
+++ StoryScape-FrontEnd/src/components/uploadForm/locationInput/locationInput.jsx
@@ -2,17 +2,34 @@ import './locationInput.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../../navBar/navBar';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// import { useState } from 'react';
-// import PlacesAutocomplete, {geocodeByAddress,getLatLng} from 'react-places-autocomplete';
-// import {
-//   useJsApiLoader
-// } from '@react-google-maps/api'
+import { useState, useRef } from 'react';
+
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Autocomplete
+} from '@react-google-maps/api'
+import Container from 'react-bootstrap/esm/Container';
+
+// this center is the center of the map and is currently set to Birmingham
+const center = { lat: 52.476992, lng: -1.9026911 }
 
 function LocationInput() {
-// const {isLoaded} = useJsApiLoader({
-//   googleMapsApiKey: process.env.VITE_GOOGLE_MAP_KEY,
-//   libraries: ['places']
-// })
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAP_KEY,
+    libraries: ['places'],
+  })
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+
+  const originRef = useRef()
+
+
+  if (!isLoaded) {
+    return <div className="spinner-border" role="status">
+    <span className="sr-only">Loading...</span>
+  </div>
+  }
 
   return (
     <>
@@ -20,8 +37,25 @@ function LocationInput() {
     
     <div className="location-search-input">
     <i className="bi bi-cursor-fill" id='nav-icon'></i>
-    <input type="text" className="form-control" id="location-input" aria-describedby="locationInput" placeholder="Search Places ..."/>
+    <Autocomplete>
+    <input type="text" className="form-control" id="location-input" aria-describedby="locationInput" placeholder="Search Places ..." ref={originRef}/>
+    </Autocomplete>
     <button className='search-location-btn'><i className="bi bi-search" id='search-icon'></i></button>
+    {/* The Container for the map  */}
+    <Container > 
+    <GoogleMap
+          center={center}
+          zoom={10}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          options={{
+            zoomControl: true,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: true,
+          }}
+          onLoad={map => setMap(map)}
+        >
+        </GoogleMap></Container>
   
     </div>
 
