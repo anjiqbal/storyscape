efaac5df532e0f1c186fba22f30b6a7ec0124bef
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-06-20 15:49:48 +0100

    edited vite.config.js

--- StoryScape-FrontEnd/vite.config.js
+++ StoryScape-FrontEnd/vite.config.js
@@ -1,14 +1,14 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export default {
-  // your Vite config options
-};
+// export default {
+//   // your Vite config options
+// };
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
