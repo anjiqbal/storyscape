637eb63ed46bbcdc7ce52ad4cb17197a5f6e3b1e
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-06-21 11:45:26 +0100

    added handlesubmit function

--- StoryScape-FrontEnd/src/components/uploadForm/uploadForm.jsx
+++ StoryScape-FrontEnd/src/components/uploadForm/uploadForm.jsx
@@ -15,16 +15,15 @@ function UploadForm() {
   const [story, setStory] = useState("");
   // const [location, setLocation] = useState("");
   const [date, setDate] = useState("");
+  const [storyObject, setStoryObject] = useState({});
 
   function handleTitle(input) {
     setTitle(input);
   }
 
-
   function handleStoryDescription(input) {
     setStoryDescription(input);
   }
-
 
   function handleStory(input) {
     setStory(input);
@@ -37,13 +36,21 @@ function UploadForm() {
   function handleDate(input) {
     setDate(input);
   }
-console.log(date)
-console.log(story)
-console.log(storyDescription) 
-console.log(title)
+  function handleSubmit(event) {
+    event.preventDefault();
 
+    const newStoryObject = {
+      user_id: 29035792,
+      story_title: title,
+      story_description: storyDescription,
+      story_location: "Birmingham",
+      story_date: date,
+      story_main: story,
+    };
+    setStoryObject(newStoryObject);
+  }
 
- 
+  console.log(storyObject);
 
   return (
     <>
@@ -53,53 +60,58 @@ console.log(title)
       <div className="user-upload-outer">
         <h1>Share your story with the world</h1>
         <div className="user-upload-container">
-          <Row className="title-row">
-            <div className="input-container">
-              <label>Story Title:</label>
-              <input
-                type="text"
-                placeholder="Growing up in Birmingham in the 40's"
-                onChange={(event) => {
-                  handleTitle(event.target.value);
-                }}
-              />
-            </div>
-          </Row>
-          <Row className="description-row">
-            <div className="input-container">
-              <label>Story Description:</label>
-             
-              <textarea
-                type="text"
-                placeholder="Growing up in Birmingham in the 40's"
-                onChange = {(event) => { handleStoryDescription(event.target.value)}}
-              />
-            </div>
-          </Row>
-          <Row className="location-row">
-            <div className="input-container">
-              <label>Story Location:</label>
-              <LocationInput />
-            </div>
-          </Row>
-          <Row className="date-row">
-            <div className="input-container">
-              <label>Story date</label>
-              <DatePickerComponent handleDate = {handleDate} />
-            </div>
-          </Row>
+          <form onSubmit={handleSubmit}>
+            <Row className="title-row">
+              <div className="input-container">
+                <label>Story Title:</label>
+                <input
+                  type="text"
+                  placeholder="Growing up in Birmingham in the 40's"
+                  onChange={(event) => {
+                    handleTitle(event.target.value);
+                  }}
+                />
+              </div>
+            </Row>
+            <Row className="description-row">
+              <div className="input-container">
+                <label>Story Description:</label>
 
-          <Row className="story-text-row">
-            <div className="input-container">
-              <label>Write your story:</label>
-              <textarea
-                type="textarea"
-                placeholder="Growing up in Birmingham in the 40's"
-                onChange = {(event) => { handleStory(event.target.value)}}
-              />
-            </div>
-          </Row>
-          {/* <Row className="video-row">
+                <textarea
+                  type="text"
+                  placeholder="Growing up in Birmingham in the 40's"
+                  onChange={(event) => {
+                    handleStoryDescription(event.target.value);
+                  }}
+                />
+              </div>
+            </Row>
+            <Row className="location-row">
+              <div className="input-container">
+                <label>Story Location:</label>
+                <LocationInput />
+              </div>
+            </Row>
+            <Row className="date-row">
+              <div className="input-container">
+                <label>Story date</label>
+                <DatePickerComponent handleDate={handleDate} />
+              </div>
+            </Row>
+
+            <Row className="story-text-row">
+              <div className="input-container">
+                <label>Write your story:</label>
+                <textarea
+                  type="textarea"
+                  placeholder="Growing up in Birmingham in the 40's"
+                  onChange={(event) => {
+                    handleStory(event.target.value);
+                  }}
+                />
+              </div>
+            </Row>
+            {/* <Row className="video-row">
             <div className="input-container">
               <label>Upload Video</label>
               <VideoUpload />
@@ -123,13 +135,16 @@ console.log(title)
               <input type="text" placeholder="Grandad talking about his..." />
             </div>
           </Row> */}
-          <Row className="btn-row">
-            <div className="input-container">
-              <button className="draft-btn">Save Draft</button>
-              <button className="preview-btn">Preview</button>
-              <button className="upload-btn">Upload</button>
-            </div>
-          </Row>
+            <Row className="btn-row">
+              <div className="input-container">
+                <button className="draft-btn">Save Draft</button>
+                <button className="preview-btn">Preview</button>
+                <button className="upload-btn" type="submit">
+                  Upload
+                </button>
+              </div>
+            </Row>
+          </form>
         </div>
       </div>
       <Footer />
