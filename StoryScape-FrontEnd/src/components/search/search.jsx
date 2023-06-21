96822a66e5b2b47b053cb6ffbd0b9a844eddd928
abdirahmanmohamed <abdi3332@outlook.com>
2023-06-21 14:20:48 +0100

    drop down filter for countries created

--- StoryScape-FrontEnd/src/components/search/search.jsx
+++ StoryScape-FrontEnd/src/components/search/search.jsx
@@ -2,40 +2,32 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./search.css";
 import PropTypes from "prop-types";
-import countries from "./countries.jsx"
+import countries from "./countries.jsx";
 
 function Search({ setSearchTerm }) {
   const navigate = useNavigate();
   const [input, setInput] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
-  // function handleInput(input) {
-  //   setInput(input);
-  // }
-
-  //create a handleSearch function that will take in the event and loop through the countries array to find the country that matches the input
-  //if the country matches the input, then set the searchTerm to the country name
   function handleSearch(e) {
     const searchTerm = e.target.value.toLowerCase();
-    if(/\d/.test(searchTerm)){
-      setInput(searchTerm);
-    }
-    else{
-      setInput(e.target.value);
-    }
-    for (let i = 0; i < countries.length; i++) {
-      if ( e.target.value.toLowerCase() === countries[i].country) {
-        setInput(() => countries[i].country);
-      }
-      console.log(e.target.value)
+    setInput(searchTerm);
+    if (searchTerm.length >= 1) {
+      const filtered = countries.filter((country) =>
+        country.country.toLowerCase().startsWith(searchTerm)
+      );
+      setFilteredCountries(filtered);
+    } else {
+      setFilteredCountries([]);
     }
   }
-
 
   function handleSubmit(event) {
     event.preventDefault();
     navigate("/storyCollection", { state: input });
     setSearchTerm(input);
     setInput("");
+    setFilteredCountries([]);
   }
 
   function handleEnter(event) {
@@ -44,9 +36,9 @@ function Search({ setSearchTerm }) {
       navigate("/storyCollection", { state: input });
       setSearchTerm(input);
       setInput("");
+      setFilteredCountries([]);
     }
   }
-
 
   return (
     <section className="search-section ">
@@ -61,11 +53,15 @@ function Search({ setSearchTerm }) {
             placeholder="Search for a location"
             value={input}
             onChange={handleSearch}
-            // onChange={(event) => {
-            //   handleInput(event.target.value);
-            // }}
             onKeyDown={handleEnter}
           />
+          {filteredCountries.length > 0 && (
+            <ul className="search-results">
+              {filteredCountries.map((country) => (
+                <li key={country.country}>{country.country}</li>
+              ))}
+            </ul>
+          )}
           <button className="search-button" type="submit">
             Search
           </button>
