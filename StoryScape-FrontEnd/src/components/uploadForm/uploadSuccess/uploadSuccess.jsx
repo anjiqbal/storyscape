611a99368342fdef63f661f31df91ea6e8b5d9f3
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-06-22 12:32:26 +0100

    box shadow working on header but not body

--- StoryScape-FrontEnd/src/components/uploadForm/uploadSuccess/uploadSuccess.jsx
+++ StoryScape-FrontEnd/src/components/uploadForm/uploadSuccess/uploadSuccess.jsx
@@ -1,40 +1,51 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import  "./uploadSuccess.css"
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./uploadSuccess.css";
 import { useNavigate } from "react-router-dom";
-import NavBar from '../../navBar/navBar';
-import Footer from '../../Footer/footer';
-
+import NavBar from "../../navBar/navBar";
+import Footer from "../../Footer/footer";
 
 function UploadSuccess() {
   const navigate = useNavigate();
-function handleClick(event) { 
-  event.preventDefault();
-  navigate("/");}
-  
+  function handleClick(event) {
+    event.preventDefault();
+    navigate("/");
+  }
+
   return (
-    <div className="success-collection-wrapper">
-      <header>
-        <NavBar/>
-      </header>
-    <Card className="text-center">
-    <div className="success-header">
-      <Card.Header><h1 className="uploadText">Upload Successful!</h1></Card.Header>
+    <>
+      <div className="success-collection-wrapper">
+        <header>
+          <NavBar />
+        </header>
+        <div className="success-card">
+          <Card className="text-center">
+            <div className="success-header">
+              <Card.Header>
+                <h1 className="uploadText">Upload Successful!</h1>
+              </Card.Header>
+            </div>
+            <div className="success-body">
+              <Card.Body>
+                <Card.Title>
+                  <h2>Your Content is Now Available</h2>
+                </Card.Title>
+                <Card.Text>
+                  <h3>Click the button below to go back to the home page.</h3>
+                </Card.Text>
+                <Button className="successButton" onClick={handleClick}>
+                  Homepage.
+                </Button>
+              </Card.Body>
+            </div>
+          </Card>
+          </div>
+          <footer>
+            <Footer />
+          </footer>
+        
       </div>
-      <div className="success-body">
-      <Card.Body>
-        <Card.Title><h2>Your Content is Now Available</h2></Card.Title>
-        <Card.Text>
-         <h3>Click the button below to go back to the home page.</h3> 
-        </Card.Text>
-        <Button className="successButton" onClick={handleClick}>Homepage.</Button>
-      </Card.Body>
-      </div>
-    </Card>
-    <footer>
-    <Footer/>
-    </footer>
-    </div>
+    </>
   );
 }
 
