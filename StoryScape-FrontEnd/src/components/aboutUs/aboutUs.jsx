a33e92caae2528543a95f4caf5582728384fc8de
Trip25 <t.gurung1995@gmail.com>
2023-06-24 22:23:45 +0100

    added team members to about us page

--- StoryScape-FrontEnd/src/components/aboutUs/aboutUs.jsx
+++ StoryScape-FrontEnd/src/components/aboutUs/aboutUs.jsx
@@ -1,16 +1,28 @@
 import NavBar from "../navBar/navBar";
 import weDaBest from "./djk.png";
 import './aboutUs.css'
+import Container from 'react-bootstrap/esm/Container'
+
 export default function aboutUs() {
   return (
     <>
       <header>
         <NavBar />
       </header>
+      <Container className= 'about-us'>
       <div className="wedabest">
-      <h1>We gonna win more. We gonna live more. We the best.</h1>
+      <h2>We are System 32</h2>
+      <h3>Our team consists of 6 members:</h3>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Abdiraman Mohamed</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Akiel Chaudry</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Anjum Iqbal</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Jay Kirtlan</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Qasim Mohammed</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Tripti Gurung</a></h5>
+
       <img src={weDaBest} />
       </div>
+      </Container>
     </>
   );
 }
