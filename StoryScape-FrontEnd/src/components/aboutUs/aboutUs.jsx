9d9304fae381f76ae53992bf21f24a861294875e
Trip25 <t.gurung1995@gmail.com>
2023-06-24 23:28:48 +0100

    added linkedin anchors for team and nav in the about us page

--- StoryScape-FrontEnd/src/components/aboutUs/aboutUs.jsx
+++ StoryScape-FrontEnd/src/components/aboutUs/aboutUs.jsx
@@ -1,4 +1,5 @@
 import NavBar from "../navBar/navBar";
+import Footer from "../footer/footer";
 import weDaBest from "./djk.png";
 import './aboutUs.css'
 import Container from 'react-bootstrap/esm/Container'
@@ -6,6 +7,7 @@ import Container from 'react-bootstrap/esm/Container'
 export default function aboutUs() {
   return (
     <>
+    <div className= 'about-us-page'>
       <header>
         <NavBar />
       </header>
@@ -13,16 +15,18 @@ export default function aboutUs() {
       <div className="wedabest">
       <h2>We are System 32</h2>
       <h3>Our team consists of 6 members:</h3>
-      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Abdiraman Mohamed</a></h5>
-      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Akiel Chaudry</a></h5>
-      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Anjum Iqbal</a></h5>
-      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Jay Kirtlan</a></h5>
-      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Qasim Mohammed</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/abdirahmanmohamed10/" target="_blank">&nbsp;Abdiraman Mohamed</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/akiel-chaudry-6666bb156/" target="_blank">&nbsp;Akiel Chaudry</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/anjum--iqbal/" target="_blank">&nbsp;Anjum Iqbal</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/jay-kirtlan-juniorfullstackdevuk/" target="_blank">&nbsp;Jay Kirtlan</a></h5>
+      <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/qasim-mohammed-65251517b/" target="_blank">&nbsp;Qasim Mohammed</a></h5>
       <h5><i className="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/tripti-g-294089a6/" target="_blank">&nbsp;Tripti Gurung</a></h5>
 
       <img src={weDaBest} />
       </div>
       </Container>
+    </div>
+      <Footer />
     </>
   );
 }
