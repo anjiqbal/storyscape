8cafb0d0962c234bc355f561c2e328778c86222b
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-06-24 08:37:55 +0100

    footer css

--- StoryScape-FrontEnd/src/components/search/search.jsx
+++ StoryScape-FrontEnd/src/components/search/search.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 import countries from "./countries.jsx";
 import supabase from "../../config/supabaseClient";
 
-function Search({ setSearchTerm }) {
+function Search({ setSearchTerm, showDescription, isFooter }) {
   const navigate = useNavigate();
   const [input, setInput] = useState("");
 
@@ -29,6 +29,48 @@ function Search({ setSearchTerm }) {
       setInput("");
     }
   }
+
+  return (
+    <section className={`search-section ${isFooter ? "footer-search-section" : ""}`}>
+      <form onSubmit={handleSubmit}>
+        {showDescription && (
+          <p className="description search-description">
+            Discover stories from around the world
+          </p>
+        )}
+        <div id="input-container">
+          <select
+            className="search-input"
+            value={input}
+            onChange={handleCountry}
+            onKeyDown={handleEnter}
+          >
+           <option value="">Search for a country</option> {/* Default empty option */}
+            {countries.map((country) => (
+              <option key={country.country} value={country.country}>
+                {country.country}
+              </option>
+            ))}
+          </select>
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+Search.propTypes = {
+  setSearchTerm: PropTypes.func.isRequired,
+  showDescription: PropTypes.bool.isRequired,
+  isFooter: PropTypes.bool.isRequired,
+};
+
+export default Search;
+
+
+
   // async function handleSubmit(event) {
   //     event.preventDefault();
 
@@ -69,37 +111,3 @@ function Search({ setSearchTerm }) {
   //     setInput("");
   //   }
   // }
-
-  return (
-    <section className="search-section">
-      <form onSubmit={handleSubmit}>
-        <p className="description search-description">
-          Discover stories from around the world
-        </p>
-        <div id="input-container">
-          <select
-            className="search-input"
-            value={input}
-            onChange={handleCountry}
-            onKeyDown={handleEnter}
-          >
-            {countries.map((country) => (
-              <option key={country.country} value={country.country}>
-                {country.country}
-              </option>
-            ))}
-          </select>
-          <button className="search-button" type="submit">
-            Search
-          </button>
-        </div>
-      </form>
-    </section>
-  );
-}
-
-Search.propTypes = {
-  setSearchTerm: PropTypes.func.isRequired,
-};
-
-export default Search;
