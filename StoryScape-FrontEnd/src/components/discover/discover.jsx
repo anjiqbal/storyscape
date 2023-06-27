6fff9d2b9e70a76a99213653fa45d71130cad2e4
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-06-27 08:14:33 +0100

    updated discover page

--- StoryScape-FrontEnd/src/components/discover/discover.jsx
+++ StoryScape-FrontEnd/src/components/discover/discover.jsx
@@ -1,14 +1,68 @@
+import { useState, useEffect } from "react";
+import supabase from "../../config/supabaseClient.jsx";
 import NavBar from "../navBar/navBar";
+import Footer from "../Footer/footer";
+import StoryCard from "../storyCollection/storyCard/storyCard.jsx";
+import { Row, Col} from "react-bootstrap";
+import "./discover.css";
 
 export default function Discover() {
-  return (
-    <>
-    <NavBar />
+  const [result, setResult] = useState([]);
 
-    <div>Discover</div>
-    </>
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("stories").select();
+
+      if (data !== null) {
+        setResult(data);
+      }
+
+      if (error) {
+        alert("error");
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+
+  return (
+    <div className="story-collection-wrapper">
+      <header>
+        <NavBar />
+      </header>
+      <>
+        {result && (
+          <main className="story-collection-content">
+            <Row className="h-100">
+              {result.length === 0 ? (
+                <Col>
+                  <p>No matching stories found.</p>
+                </Col>
+              ) : (
+                result.map((story) => (
+                  <Col sm={12} md={4} key={story.story_id}>
+                    <StoryCard
+                      result={result}
+                      storyId={story.story_id}
+                      storyTitle={story.story_title}
+                      storyLocation={story.story_continent}
+                      storyDescription={story.story_description}
+                    />
+                  </Col>
+                ))
+              )}
+            </Row>
+          </main>
+        )}
+      </>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
-
-
-
