e1449f5319ab5a333161daa578ba1fb7c918bd9d
AkielCh <akiel98@hotmail.com>
2023-06-28 13:38:06 +0100

    Story now can be refreshed

--- StoryScape-FrontEnd/src/components/storyCollection/story/story.jsx
+++ StoryScape-FrontEnd/src/components/storyCollection/story/story.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Story.css";
 import NavBar from "../../navBar/navBar";
@@ -5,65 +6,84 @@ import Footer from "../../Footer/footer";
 import { Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import placeholderImage from "../../homePageStory/images/StoryScape_placeholder2.png";
+import supabase from "../../../../src/config/supabaseClient";
+
+
 
 export default function StoryPage() {
   const location = useLocation();
-  const {
-    storyId,
-    storyImage,
-    storyTitle,
-    storyContinent,
-    storyDescription,
-    storyMain,
-    storyLocation,
-    storyDate,
-  } = location.state;
-  console.log(`THIS IS THE URL ${storyImage}`);
-  console.log(location.state);
+  const { storyId } = location.state;
+  const [storyData, setStoryData] = useState(null);
+
+  useEffect(() => {
+    const fetchStoryData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("stories")
+          .select()
+          .eq("story_id", storyId)
+          .single();
+
+        if (error) {
+          console.error(error);
+        } else {
+          setStoryData(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchStoryData();
+  }, [storyId]);
+
   return (
     <>
       <div className="entireContainer">
         <header>
-          {" "}
-          <NavBar />{" "}
+          <NavBar />
         </header>
         <Container className="mainStoryContainer">
-          <h2 className="mainStoryTitle">{storyTitle}</h2>
+          {storyData ? (
+            <>
+              <h2 className="mainStoryTitle">{storyData.story_title}</h2>
 
-          <Container className="imageContainerMain">
-            {storyImage && (
-              <Container className="mainStoryImageContainer">
-                <img
-                  src={storyImage}
-                  alt="StoryImage"
-                  className="mainStoryImage"
-                />
+              <Container className="imageContainerMain">
+                {storyData.story_url ? (
+                  <Container className="mainStoryImageContainer">
+                    <img
+                      src={storyData.story_url}
+                      alt="StoryImage"
+                      className="mainStoryImage"
+                    />
+                  </Container>
+                ) : (
+                  <Container className="mainStoryImageContainer">
+                    <img
+                      src={placeholderImage}
+                      alt="PlaceholderImage"
+                      className="mainStoryImage"
+                    />
+                  </Container>
+                )}
               </Container>
-            )}
-            {!storyImage && (
-              <Container className="mainStoryImageContainer">
-                <img
-                  src={placeholderImage}
-                  alt="PlaceholderImage"
-                  className="mainStoryImage"
-                />
+              <Container className="main-story-content">
+                <p className="mainStoryContinent">{storyData.story_continent}</p>
+
+                <p className="storyLocation">{storyData.story_country}</p>
+
+                <p className="storyDate">{storyData.story_date}</p>
+
+                <p className="mainStoryMain">{storyData.story_main}</p>
               </Container>
-            )}
-          </Container>
-          <Container className="main-story-content">
-            <p className="mainStoryContinent">{storyContinent}</p>
-
-            <p className="storyLocation">{storyLocation}</p>
-
-            <p className="storyDate">{storyDate}</p>
-
-            <p className="mainStoryMain">{storyMain}</p>
-          </Container>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
         </Container>
 
         <footer>
-          {" "}
-          <Footer />{" "}
+          <Footer />
         </footer>
       </div>
     </>
