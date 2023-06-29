4e8dca6893ba5eb0041be889f19b06dabd6b21dd
AkielCh <akiel98@hotmail.com>
2023-06-29 10:09:01 +0100

    Filter now included

--- StoryScape-FrontEnd/src/components/storyCollection/storyCollection.jsx
+++ StoryScape-FrontEnd/src/components/storyCollection/storyCollection.jsx
@@ -1,21 +1,22 @@
 import { useState, useEffect } from "react";
 import StoryCard from "./storyCard/storyCard";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import Footer from "../Footer/footer";
 import { useLocation, useParams } from "react-router-dom";
 import NavBar from "../navBar/navBar";
 import "./storyCollection.css";
 import supabase from "../../config/supabaseClient";
-import "./../homePageStory/homePageStory.css"
-
+import "./../homePageStory/homePageStory.css";
 
 export default function StoryCollection() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const input = searchParams.get("continent");
-  // console.log(searchParams)
-  
+
   const [result, setResult] = useState([]);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,28 +25,38 @@ export default function StoryCollection() {
         .select()
         .eq("story_continent", input);
 
-      if (!data && isLoading ) {
+      if (!data && isLoading) {
         return <div>Loading...</div>;
       }
 
       if (data !== null) {
         setResult(data);
+        setFilteredResults(data);
       }
 
       if (error) {
         alert("error");
-        // console.log(error);
       }
     };
 
     fetchData();
   }, [input]);
 
-  console.log(location);
+  const filterResultsByDateRange = () => {
+    const filteredResults = result.filter((story) => {
+      const storyDate = new Date(story.story_date);
+      return (
+        storyDate >= new Date(startDate) &&
+        storyDate <= new Date(endDate)
+      );
+    });
 
-  useEffect(() => {
-    // console.log(result); // Log the updated result state
-  }, [result]);
+    setFilteredResults(filteredResults);
+  };
+
+  const handleFilterClick = () => {
+    filterResultsByDateRange();
+  };
 
   return (
     <div className="story-collection-wrapper">
@@ -53,15 +64,36 @@ export default function StoryCollection() {
         <NavBar />
       </header>
       <>
-        {result && (
+        <div className="date-range-picker-wrapper">
+          <div>
+            <label htmlFor="start-date">Start Date:</label>
+            <input
+              type="date"
+              id="start-date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="end-date">End Date:</label>
+            <input
+              type="date"
+              id="end-date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+          <Button onClick={handleFilterClick}>Filter</Button>
+        </div>
+        {filteredResults && (
           <main className="story-collection-content">
             <Row className="h-100">
-              {result!==null && result.length === 0 ? (
+              {filteredResults.length === 0 ? (
                 <Col>
                   <p>No matching stories found.</p>
                 </Col>
               ) : (
-                result.map((story) => (
+                filteredResults.map((story) => (
                   <Col xs={12} sm={12} md={6} lg={4} key={story.story_id}>
                     <StoryCard
                       result={result}
@@ -87,4 +119,3 @@ export default function StoryCollection() {
     </div>
   );
 }
-
