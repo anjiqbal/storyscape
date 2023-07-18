39b30566e9fcc45026aa6e964ffa2242c132cff4
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-07-18 22:05:55 +0100

    updated app and footer code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,35 +2,27 @@ import { useState, useEffect } from "react";
 import Search from "../search/search";
 import Navbar from "../navBar/navBar";
 import Footer from "../Footer/footer";
-import { useNavigate , useLocation} from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import "./app.css";
 import StoryMission from "../storyMissionCard/storyMission";
 import HomePageStory from "../homePageStory/homePageStory";
 import supabase from "../../config/supabaseClient.jsx";
-import StoryCard from "../storyCollection/storyCard/storyCard.jsx";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
   const [fetchError, setFetchError] = useState(null);
   const [stories, setStories] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
-  const {loggedIn} = location.state || {loggedIn:false};
-  // const [loggedIn, setLoggedIn] = useState(false);
-
-  const [userData, setUserData] = useState([]); // State to store the fetched user data
-  // console.log(supabase);
+  const { loggedIn } = location.state || { loggedIn: false };
 
   function handleUploadButton(event) {
     event.preventDefault();
     if (!loggedIn) {
       alert("Please login to upload a story");
-      navigate("/signUp",{state: {loggedIn}});
+      navigate("/signUp", { state: { loggedIn } });
       return;
-    }
-    else
-    navigate("/uploadForm",{state: {loggedIn}});
+    } else navigate("/uploadForm", { state: { loggedIn } });
   }
 
   useEffect(() => {
@@ -50,27 +42,18 @@ function App() {
 
     fetchData();
   }, []);
-console.log(loggedIn)
-  //FOR DARK MODE
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => setDarkMode(!darkMode);
+ 
   return (
     <main className={`app ${darkMode ? "dark-mode" : ""}`}>
-      {/* Navbar section */}
       <header>
         <Navbar />
-        {/* <button onClick={toggleDarkMode} className="dark-btn">
-          Toggle Dark Mode
-        </button> */}
       </header>
       <div className="filler"></div>
       <div className="first-row">
         <Row>
-          {/* StoryMission component */}
           <Col xs={12} s={12} md={12} lg={6} className="home-storymission">
             <StoryMission />
           </Col>
-          {/* Search component and Upload button */}
           <Col xs={12} s={12} md={12} lg={6}>
             <Row>
               <div id="form-container">
@@ -81,16 +64,16 @@ console.log(loggedIn)
               </div>
             </Row>
             <Row>
-              
-                <section className="upload-form">
-                  <form onSubmit={handleUploadButton}>
-                  <h2 className="homepage-input-headings">Or, upload your own...</h2>
-                    <button type="submit" className="upload-story-btn">
-                      Upload
-                    </button>
-                  </form>
-                </section>
-             
+              <section className="upload-form">
+                <form onSubmit={handleUploadButton}>
+                  <h2 className="homepage-input-headings">
+                    Or, upload your own...
+                  </h2>
+                  <button type="submit" className="upload-story-btn">
+                    Upload
+                  </button>
+                </form>
+              </section>
             </Row>
           </Col>
         </Row>
@@ -99,9 +82,8 @@ console.log(loggedIn)
       <div className="second-row">
         <h3 className="trending-title">Trending on StoryScape</h3>
         <Row>
-          {/* HomePageStory components */}
           <Col sm={12} md={4}>
-          <HomePageStory stories={stories} />
+            <HomePageStory stories={stories} />
           </Col>
           <Col sm={12} md={4}>
             <HomePageStory stories={stories} />
